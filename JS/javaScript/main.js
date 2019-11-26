3f5524351b3f957b638119b719e117ff680cbe35
Ke Gong <gongke@cn.ibm.com>
2019-11-26 16:05:25 +0800

    update code

---
+++ JS/javaScript/main.js
@@ -0,0 +1,12 @@
+var Human = {
+    getHeight: function () {
+        return this.height
+    },
+    setHeight: function (height) {
+        this.height = height
+    }
+}
+
+
+Human.setHeight(180)
+alert(Human.height)
